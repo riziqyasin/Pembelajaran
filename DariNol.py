886c88387395e01e8c85b8c5bb8fecf7248b5005
riziqyasin <riziqyasin@gmail.com>
2026-04-21 17:55:11 +0700

    Update DariNol.py

--- DariNol.py
+++ DariNol.py
@@ -1,1 +1,38 @@
-TEST
+# # # #Jadi Hari Ini Rencananya Mau Belajar Sederhana Aja Terkait Fungsi Variable
+
+print("hello world") #ini merupakan contoh sederhana dari dunia coding
+
+# # # #------------------------------Variable----------------------------------------
+
+# # #contoh kita ingin memanggil variable nama, maka
+nama = input("nama kamu siapa?") #penggunaan code input itu digunakan dari user, yang dimana digunakan untuk memanggil
+umur = input("umur kamu berapa?")
+
+# # print(f'nama saya{nama}dan{umur}') #f Cara paling simpel dan modern buat gabungin tulisan sama isi variabel
+
+# # #Multiple Variable
+x, y, z = "AKU" , "SAYANG" , "KAMU"
+print(x)
+print(y)
+print(z)
+
+#================================FUNCTION================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
